cb6f364be0dc5f28ec6ee0e89129305d034d1b3b
tangshuaibo <tangshuaibo@ghzq.com>
2023-03-07 16:32:29 +0800

    继续提交

--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+const app = createApp(App);
 
-createApp(App).use(store).use(router).mount("#app");
+app.use(store);
+app.use(router);
+app.use(ElementPlus);
+app.mount("#app");
---
+++ vue.config.js
@@ -0,0 +1,18 @@
+// vue.config.js
+module.exports = {
+  // 选项
+  // 基本路径
+  publicPath: "/",
+  // 构建时输出的目录
+  outputDir: "dist",
+  // 静态文件存放的目录(js、css、img、fonts)
+  assetsDir: "static",
+  // html的输出路径
+  indexPath: "index.html",
+  // 文件名哈希
+  filenameHashing: true,
+  //pages在multi-page(多页)模式下构建应用。每个"page"应该有一个对应的javascript入口文件
+  devServer:{
+    
+  }
+};
